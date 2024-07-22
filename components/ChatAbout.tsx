24cd77b3bad0e8b7d7816af2462ea1755988b834
MadhurThareja01 <madhurthareja1105@gmail.com>
2024-07-22 22:47:19 +0530

    created vartalaap

---
+++ components/ChatAbout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ChatAbout() {
+	return (
+		<div className="flex-1 flex items-center justify-center">
+			<div className="text-center space-y-5">
+				<h1 className="text-3xl font-bold">वार्तालाप चैट में आपका स्वागत है</h1>
+				<p className="w-96">
+					This is a chat application made with love by Madhur Thareja. 
+					Login to send message
+				</p>
+			</div>
+		</div>
+	);
+}
